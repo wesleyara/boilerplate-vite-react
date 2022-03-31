5f1cbb1b65b17d7e687d82bc59ba52e8a43cf686
wesleyara <wes1gom@gmail.com>
2022-03-31 16:35:23 -0300

    feat: Adicionando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
+import { GlobalStyles } from "./styles/global";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
